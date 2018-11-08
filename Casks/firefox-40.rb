256d89c59cf81c648c0a5ffb938d9958b33388dd
Slava Boiko <slava.boiko@outlook.com>
2018-11-08 11:01:17 +0000

    Update firefox-40 to comply with latest homebrew cask spec

--- Casks/firefox-40.rb
+++ Casks/firefox-40.rb
@@ -3,10 +3,9 @@ cask 'firefox-40' do
   sha256 '9e6ba26c793a0dca73b46f880e4bd722be602272a624a83a2df8ff65946d7636'
 
   url "https://ftp.mozilla.org/pub/firefox/releases/#{version}/mac/en-US/Firefox%20#{version}.dmg"
-  name 'Firefox'
+  appcast 'https://www.mozilla.org/en-US/firefox/releases/'
   name 'Mozilla Firefox 40.0'
-  homepage 'https://www.mozilla.org/en-US/firefox/'
-  tags :vendor => 'Mozilla'
-
+  homepage 'https://www.mozilla.org/firefox/'
+  
   app 'Firefox.app', :target => 'Firefox-40.app'
 end
