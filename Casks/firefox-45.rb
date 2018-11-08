6c889463be000808b55be5cfaa0be9392e771a06
Slava Boiko <slava.boiko@outlook.com>
2018-11-08 11:54:41 +0000

    Update version 45

--- Casks/firefox-45.rb
+++ Casks/firefox-45.rb
@@ -1,10 +1,10 @@
 cask 'firefox-45' do
-  version '45.9.0'
-  sha256 'ff75302b2d938162f986b31e6fac419ebfafda7835eaf7ca4ca1e4e7290ef5c8'
+  version '45.9.0esr'
+  sha256 'c18e758fdedf1931e19017c66f3c03321ab9ade36906535a8cae6248c723fba0'
 
   url "https://ftp.mozilla.org/pub/firefox/releases/#{version}/mac/en-US/Firefox%20#{version}.dmg"
   appcast 'https://www.mozilla.org/en-US/firefox/releases/'
-  name 'Mozilla Firefox 45.9.0'
+  name 'Mozilla Firefox 45.9.0esr'
   homepage 'https://www.mozilla.org/firefox/'
 
   app 'Firefox.app', :target => 'Firefox-45.app'
